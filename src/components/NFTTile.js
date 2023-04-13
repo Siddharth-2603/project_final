d95b57df5bc06acad2657aecede2ceb415a542f0
Siddharth-2603 <95608891+Siddharth-2603@users.noreply.github.com>
2023-04-13 12:27:49 +0530

    Update NFTTile.js

--- src/components/NFTTile.js
+++ src/components/NFTTile.js
@@ -11,11 +11,11 @@ function NFTTile (data) {
     return (
         <Link to={newTo}>
         <div className="border-2 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-2xl">
-            <img src= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thepioneerwoman.com%2Fhome-lifestyle%2Fpets%2Fg36592631%2Fsmall-fluffy-dog-breeds%2F&psig=AOvVaw2qNDT03vYnnwhaszB-1X_W&ust=1681453653573000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMCa8Y-dpv4CFQAAAAAdAAAAABAQ" alt="" className="w-72 h-80 rounded-lg object-cover" />
+            <img src= {data.data.image} alt="" className="w-72 h-80 rounded-lg object-cover" />
             <div className= "text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
                 <strong className="text-xl">{data.data.name}</strong>
                 <p className="display-inline">
-                   First NFT by builders
+                    {data.data.description}
                 </p>
             </div>
         </div>
